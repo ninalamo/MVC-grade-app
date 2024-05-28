2a0ab9d3c27c6fb4558a1c72b46db5cf4ba454c1
Ninism <nin.alamo@outlook.com>
2024-05-28 12:04:58 +0800

    Migration

---
+++ Migrations/20240528035949_subject_enrolmentxxx.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_grade_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class subject_enrolmentxxx : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectEnrolment_SectionId",
+                table: "SubjectEnrolment",
+                column: "SectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectEnrolment_SubjectId",
+                table: "SubjectEnrolment",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubjectEnrolment_Sections_SectionId",
+                table: "SubjectEnrolment",
+                column: "SectionId",
+                principalTable: "Sections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubjectEnrolment_Subjects_SubjectId",
+                table: "SubjectEnrolment",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubjectEnrolment_Sections_SectionId",
+                table: "SubjectEnrolment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubjectEnrolment_Subjects_SubjectId",
+                table: "SubjectEnrolment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubjectEnrolment_SectionId",
+                table: "SubjectEnrolment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubjectEnrolment_SubjectId",
+                table: "SubjectEnrolment");
+        }
+    }
+}
